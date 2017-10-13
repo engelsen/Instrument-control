457c261230dfa3399b772d07d6fa7b067f4e23ac
Nils Johan Engelsen <nilsjohan.engelsen@epfl.ch>
2017-10-14 00:13:47 +0200

    1. Added linear and quadratic fits to MyFit.m 2. Cleaned up by using a set function for fit_name instead of lower in switch/case blocks 3. Fixed bug with input parser

--- @MyFit/MyFit.m
+++ @MyFit/MyFit.m
@@ -6,6 +6,7 @@ classdef MyFit < handle
         fit_name='linear'
         valid_fit_names={'linear','quadratic','gaussian','lorentzian'}
         CFitObj;
+        coeffs;
     end
     
     properties (Dependent=true)
@@ -16,7 +17,15 @@ classdef MyFit < handle
         function this=MyFit(varargin)
             createParser(this);
             parse(this.Parser,varargin{:});
+            this.Parser.Results
             parseInputs(this);
+            if ismember('DataTrace',this.Parser.UsingDefaults) &&...
+                ~ismember('x',this.Parser.UsingDefaults) &&...
+                ~ismember('y',this.Parser.UsingDefaults)
+            
+                this.DataTrace.x=this.Parser.Results.x;
+                this.DataTrace.y=this.Parser.Results.y;
+            end
         end
         
         function createParser(this)
@@ -24,6 +33,8 @@ classdef MyFit < handle
             addParameter(p,'fit_name','linear',@ischar)
             addParameter(p,'DataTrace',MyTrace());
             addParameter(p,'FitTrace',MyTrace());
+            addParameter(p,'x',[]);
+            addParameter(p,'y',[]);
             this.Parser=p;
             this.Parser
         end
@@ -31,30 +42,39 @@ classdef MyFit < handle
         %Sets the class variables to the inputs from the inputParser.
         function parseInputs(this)
             for i=1:length(this.Parser.Parameters)
-            %Takes the value from the inputParser to the appropriate
-            %property.
-                this.(this.Parser.Parameters{i})=...
-                    this.Parser.Results.(this.Parser.Parameters{i});
+                %Takes the value from the inputParser to the appropriate
+                %property.
+                if isprop(this,this.Parser.Parameters{i})
+                    this.(this.Parser.Parameters{i})=...
+                        this.Parser.Results.(this.Parser.Parameters{i});
+                end
             end
         end
         
         function set.fit_name(this,fit_name)
             assert(ischar(fit_name),'The fit name must be a string');
-            assert(ismember(lower(fit_name),this.valid_fit_names),...
-                '%s is not a supported fit name',fit_name);
-           this.fit_name=fit_name; 
+           this.fit_name=lower(fit_name); 
         end
         
-        function fitData(this)
-            this.CFitObj=fitArbFun(this.fit_function,...
-                this.DataTrace.x,this.DataTrace.y);
-            this.FitTrace.x=linspace(min(this.DataTrace.x),...
-                max(this.DataTrace.x),1e4);
-            this.FitTrace.y=this.CFitObj(this.FitTrace.x)';
+        function fitTrace(this)
+            switch this.fit_name
+                case 'linear'
+                    this.coeffs=polyfit(this.DataTrace.x,this.DataTrace.y,1);
+                case 'quadratic'
+                    this.coeffs=polyfit(this.DataTrace.x,this.DataTrace.y,2);
+                otherwise
+                    this.CFitObj=fitArbFun(this.fit_function,...
+                        this.DataTrace.x,this.DataTrace.y);
+                    this.FitTrace.x=linspace(min(this.DataTrace.x),...
+                        max(this.DataTrace.x),1e4);
+                    this.FitTrace.y=this.CFitObj(this.FitTrace.x)';
+            end
         end
 
         function fit_function=get.fit_function(this)
-           switch lower(this.fit_name)
+            assert(ismember(this.fit_name,this.valid_fit_names),...
+                '%s is not a supported fit name',this.fit_name);
+           switch this.fit_name
                case 'linear'
                    fit_function='a*x+b';
                case 'quadratic'
@@ -65,5 +85,6 @@ classdef MyFit < handle
                    fit_function='a/(pi)*(b/((x-c)^2+b^2)';
            end
         end
+        
     end
 end
