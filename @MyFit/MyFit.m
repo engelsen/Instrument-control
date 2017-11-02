5c97f5f5bca9ba5978cbfb756b2ad1537defab4e
Nils Johan Engelsen <nilsjohan.engelsen@epfl.ch>
2017-11-02 17:40:02 +0100

    1. Changed access settings of MyFit.m

--- @MyFit/MyFit.m
+++ @MyFit/MyFit.m
@@ -1,23 +1,29 @@
 classdef MyFit < handle
-    properties
-        Gui
+    properties (Access=public)
         Data;
-        Fit;
-        Parser;
-        FitStruct;
-        Fitdata;
-        fit_name='Linear'
         init_params=[];
         scale_init=[];
         lim_lower;
         lim_upper;
-        coeffs;
-        enable_gui=1;
         enable_plot;
         plot_handle;
-        hline_init;
         save_name;
         save_dir;
+    end
+    
+    properties (GetAccess=public, SetAccess=private)
+        Fit;
+        Gui;
+        Fitdata;
+        coeffs;
+        fit_name='Linear'
+    end
+    
+    properties (Access=private)
+        Parser;
+        FitStruct;
+        enable_gui=1;
+        hline_init;
     end
     
     properties (Dependent=true)
@@ -32,13 +38,12 @@ classdef MyFit < handle
         x_vec;
     end
     
-    events 
+    events
         NewFit;
     end
     
-    methods
-        %%Class functions
-        
+    %%Public methods
+    methods (Access=public)
         %Constructor function
         function this=MyFit(varargin)
             createFitStruct(this);
@@ -53,9 +58,9 @@ classdef MyFit < handle
                 this.Data.x=this.Parser.Results.x;
                 this.Data.y=this.Parser.Results.y;
             end
-
+            
             %If the data is appropriate, generates initial
-            %parameters 
+            %parameters
             if validateData(this)
                 genInitParams(this);
             else
@@ -67,11 +72,8 @@ classdef MyFit < handle
             
             if this.enable_gui
                 createGui(this);
-            end         
+            end
         end
-        
-        %Creates the GUI of MyFit, in separate file.
-        createGui(this);
         
         %Deletion function of object
         function delete(this)
@@ -86,38 +88,9 @@ classdef MyFit < handle
             if ~isempty(this.hline_init); delete(this.hline_init); end
             if ~isempty(this.Fit.hlines); delete(this.Fit.hlines{:}); end
         end
-
         %Close figure callback simply calls delete function for class
         function closeFigure(this,~,~)
             delete(this);
-        end
-        
-        %Creates parser for constructor
-        function createParser(this)
-            p=inputParser;
-            addParameter(p,'fit_name','Linear',@ischar)
-            addParameter(p,'Data',MyTrace());
-            addParameter(p,'Fit',MyTrace());
-            addParameter(p,'x',[]);
-            addParameter(p,'y',[]);
-            addParameter(p,'enable_gui',1);
-            addParameter(p,'enable_plot',0);
-            addParameter(p,'plot_handle',[]);
-            addParameter(p,'save_dir',[]);
-            addParameter(p,'save_name',[]);
-            this.Parser=p;
-        end
-        
-        %Sets the class variables to the inputs from the inputParser.
-        function parseInputs(this)
-            for i=1:length(this.Parser.Parameters)
-                %Takes the value from the inputParser to the appropriate
-                %property.
-                if isprop(this,this.Parser.Parameters{i})
-                    this.(this.Parser.Parameters{i})=...
-                        this.Parser.Results.(this.Parser.Parameters{i});
-                end
-            end
         end
         
         %Fits the trace using currently set parameters, depending on the
@@ -148,120 +121,6 @@ classdef MyFit < handle
             triggerNewFit(this);
         end
         
-        %Does the fit with the currently set parameters
-        function doFit(this)
-            %Fits with the below properties. Chosen for maximum accuracy.
-            this.Fitdata=fit(this.Data.x,this.Data.y,this.fit_function,...
-                'Lower',this.lim_lower,'Upper',this.lim_upper,...
-                'StartPoint',this.init_params, ....
-                'MaxFunEvals',2000,'MaxIter',2000,'TolFun',1e-9);
-            %Puts the y values of the fit into the struct.
-            this.Fit.y=this.Fitdata(this.Fit.x);
-            %Puts the coeffs into the class variable.
-            this.coeffs=coeffvalues(this.Fitdata);
-        end
-        
-        %Triggers the NewFit event such that other objects can use this to
-        %e.g. plot new fits
-        function triggerNewFit(this)
-            notify(this,'NewFit');
-        end
-        
-        %Plots the trace contained in the Fit MyTrace object.
-        function plotFit(this,varargin)
-            this.Fit.plotTrace(this.plot_handle,varargin{:});
-        end
-        
-        %Clears the plots
-        function clearFit(this)
-            cellfun(@(x) delete(x), this.Fit.hlines);
-            this.Fit.hlines={};
-        end
-        %Creates the struct used to get all things relevant to the fit
-        %model
-        function createFitStruct(this)
-            %Adds fits
-            addFit(this,'Linear','a*x+b','$$ax+b$$',{'a','b'},...
-                {'Gradient','Offset'})
-            addFit(this,'Quadratic','a*x^2+b*x+c','$$ax^2+bx+c$$',...
-                {'a','b','c'},{'Quadratic coeff.','Linear coeff.','Offset'});
-            addFit(this,'Gaussian','a*exp(-((x-c)/b)^2/2)+d',...
-                '$$ae^{-\frac{(x-c)^2}{2b^2}}+d$$',{'a','b','c','d'},...
-                {'Amplitude','Width','Center','Offset'});
-            addFit(this,'Lorentzian','a/((x-c)^2+(b/2)^2)+d',...
-                '$$\frac{a}{(x-c)^2+(b/2)^2}+d$$',{'a','b','c','d'},...
-                {'Amplitude','Width','Center','Offset'});
-            addFit(this,'Exponential','a*exp(b*x)+c',...
-                '$$ae^{bx}+c$$',{'a','b','c'},...
-                {'Amplitude','Rate','Offset'});
-        end
-        
-        %Updates the GUI if the edit or slider boxes are changed from
-        %elsewhere.
-        function updateGui(this)
-            %Converts the scale variable to the value between 0 and 100
-            %necessary for the slider
-            slider_vals=25*log10(this.scale_init)+50;
-            for i=1:this.n_params
-                set(this.Gui.(sprintf('Edit_%s',this.fit_params{i})),...
-                    'String',sprintf('%3.3e',this.scaled_params(i)));
-                set(this.Gui.(sprintf('Slider_%s',this.fit_params{i})),...
-                    'Value',slider_vals(i));
-            end
-        end
-        
-        %Adds a fit to the list of fits
-        function addFit(this,fit_name,fit_function,fit_tex,fit_params,...
-                fit_param_names)
-            this.FitStruct.(fit_name).fit_function=fit_function;
-            this.FitStruct.(fit_name).fit_tex=fit_tex;
-            this.FitStruct.(fit_name).fit_params=fit_params;
-            this.FitStruct.(fit_name).fit_param_names=fit_param_names;
-            %Generates the anonymous fit function from the above
-            args=['@(x,', strjoin(fit_params,','),')'];
-            anon_fit_fun=str2func(vectorize([args,fit_function]));
-            this.FitStruct.(fit_name).anon_fit_fun=anon_fit_fun;         
-        end
-        
-        %Generates model-dependent initial parameters, lower and upper
-        %boundaries.
-        function genInitParams(this)
-            switch this.fit_name
-                case 'Exponential'
-                    [this.init_params,this.lim_lower,this.lim_upper]=...
-                    initParamExponential(this.Data.x,this.Data.y);
-                case 'Gaussian'
-                    [this.init_params,this.lim_lower,this.lim_upper]=...
-                    initParamGaussian(this.Data.x,this.Data.y);
-                case 'Lorentzian'
-                    [this.init_params,this.lim_lower,this.lim_upper]=...
-                        initParamLorentzian(this.Data.x,this.Data.y);
-                otherwise
-                    this.init_params=ones(1,this.n_params);
-            end
-        end
-         %Checks if the class is ready to perform a fit
-        function bool=validateData(this)
-            bool=~isempty(this.Data.x) && ~isempty(this.Data.y) && ...
-                length(this.Data.x)==length(this.Data.y) && ...
-                length(this.Data.x)>=this.n_params;
-        end
-        
-        %Function for plotting fit model with current initial parameters.
-        function plotInitFun(this)
-            %Substantially faster than any alternative - generating 
-            %anonymous functions is very cpu intensive. 
-            
-            input_cell=num2cell(this.scaled_params);
-            y_vec=feval(this.FitStruct.(this.fit_name).anon_fit_fun,...
-                this.x_vec,input_cell{:});
-            if isempty(this.hline_init)
-                this.hline_init=plot(this.plot_handle,this.x_vec,y_vec);
-            else
-                set(this.hline_init,'XData',this.x_vec,'YData',y_vec);
-            end
-        end
-        
         %% Callbacks
         %Save function callback
         function saveCallback(this,~,~)
@@ -270,7 +129,7 @@ classdef MyFit < handle
                 ['Save directory is not specified.',...
                 ' Should be of type char but is %s.'], ...
                 class(this.save_dir))
-            try 
+            try
                 this.Fit.save('name',this.save_name,...
                     'save_dir',this.save_dir)
             catch
@@ -331,6 +190,154 @@ classdef MyFit < handle
             updateGui(this);
         end
         
+        %Generates model-dependent initial parameters, lower and upper
+        %boundaries.
+        function genInitParams(this)
+            switch this.fit_name
+                case 'Exponential'
+                    [this.init_params,this.lim_lower,this.lim_upper]=...
+                        initParamExponential(this.Data.x,this.Data.y);
+                case 'Gaussian'
+                    [this.init_params,this.lim_lower,this.lim_upper]=...
+                        initParamGaussian(this.Data.x,this.Data.y);
+                case 'Lorentzian'
+                    [this.init_params,this.lim_lower,this.lim_upper]=...
+                        initParamLorentzian(this.Data.x,this.Data.y);
+                otherwise
+                    this.init_params=ones(1,this.n_params);
+            end
+        end
+        
+        %Plots the trace contained in the Fit MyTrace object.
+        function plotFit(this,varargin)
+            this.Fit.plotTrace(this.plot_handle,varargin{:});
+        end
+                
+        %Clears the plots
+        function clearFit(this)
+            cellfun(@(x) delete(x), this.Fit.hlines);
+            this.Fit.hlines={};
+        end
+                
+        %Function for plotting fit model with current initial parameters.
+        function plotInitFun(this)
+            %Substantially faster than any alternative - generating
+            %anonymous functions is very cpu intensive.
+            
+            input_cell=num2cell(this.scaled_params);
+            y_vec=feval(this.FitStruct.(this.fit_name).anon_fit_fun,...
+                this.x_vec,input_cell{:});
+            if isempty(this.hline_init)
+                this.hline_init=plot(this.plot_handle,this.x_vec,y_vec);
+            else
+                set(this.hline_init,'XData',this.x_vec,'YData',y_vec);
+            end
+        end
+    end
+    
+    methods(Access=private)
+        %Creates the GUI of MyFit, in separate file.
+        createGui(this);
+        
+        %Creates parser for constructor
+        function createParser(this)
+            p=inputParser;
+            addParameter(p,'fit_name','Linear',@ischar)
+            addParameter(p,'Data',MyTrace());
+            addParameter(p,'Fit',MyTrace());
+            addParameter(p,'x',[]);
+            addParameter(p,'y',[]);
+            addParameter(p,'enable_gui',1);
+            addParameter(p,'enable_plot',0);
+            addParameter(p,'plot_handle',[]);
+            addParameter(p,'save_dir',[]);
+            addParameter(p,'save_name',[]);
+            this.Parser=p;
+        end
+        
+        %Sets the class variables to the inputs from the inputParser.
+        function parseInputs(this)
+            for i=1:length(this.Parser.Parameters)
+                %Takes the value from the inputParser to the appropriate
+                %property.
+                if isprop(this,this.Parser.Parameters{i})
+                    this.(this.Parser.Parameters{i})=...
+                        this.Parser.Results.(this.Parser.Parameters{i});
+                end
+            end
+        end
+        
+        %Does the fit with the currently set parameters
+        function doFit(this)
+            %Fits with the below properties. Chosen for maximum accuracy.
+            this.Fitdata=fit(this.Data.x,this.Data.y,this.fit_function,...
+                'Lower',this.lim_lower,'Upper',this.lim_upper,...
+                'StartPoint',this.init_params, ....
+                'MaxFunEvals',2000,'MaxIter',2000,'TolFun',1e-9);
+            %Puts the y values of the fit into the struct.
+            this.Fit.y=this.Fitdata(this.Fit.x);
+            %Puts the coeffs into the class variable.
+            this.coeffs=coeffvalues(this.Fitdata);
+        end
+        
+        %Triggers the NewFit event such that other objects can use this to
+        %e.g. plot new fits
+        function triggerNewFit(this)
+            notify(this,'NewFit');
+        end
+
+        %Creates the struct used to get all things relevant to the fit
+        %model
+        function createFitStruct(this)
+            %Adds fits
+            addFit(this,'Linear','a*x+b','$$ax+b$$',{'a','b'},...
+                {'Gradient','Offset'})
+            addFit(this,'Quadratic','a*x^2+b*x+c','$$ax^2+bx+c$$',...
+                {'a','b','c'},{'Quadratic coeff.','Linear coeff.','Offset'});
+            addFit(this,'Gaussian','a*exp(-((x-c)/b)^2/2)+d',...
+                '$$ae^{-\frac{(x-c)^2}{2b^2}}+d$$',{'a','b','c','d'},...
+                {'Amplitude','Width','Center','Offset'});
+            addFit(this,'Lorentzian','a/((x-c)^2+(b/2)^2)+d',...
+                '$$\frac{a}{(x-c)^2+(b/2)^2}+d$$',{'a','b','c','d'},...
+                {'Amplitude','Width','Center','Offset'});
+            addFit(this,'Exponential','a*exp(b*x)+c',...
+                '$$ae^{bx}+c$$',{'a','b','c'},...
+                {'Amplitude','Rate','Offset'});
+        end
+        
+        %Updates the GUI if the edit or slider boxes are changed from
+        %elsewhere.
+        function updateGui(this)
+            %Converts the scale variable to the value between 0 and 100
+            %necessary for the slider
+            slider_vals=25*log10(this.scale_init)+50;
+            for i=1:this.n_params
+                set(this.Gui.(sprintf('Edit_%s',this.fit_params{i})),...
+                    'String',sprintf('%3.3e',this.scaled_params(i)));
+                set(this.Gui.(sprintf('Slider_%s',this.fit_params{i})),...
+                    'Value',slider_vals(i));
+            end
+        end
+        
+        %Adds a fit to the list of fits
+        function addFit(this,fit_name,fit_function,fit_tex,fit_params,...
+                fit_param_names)
+            this.FitStruct.(fit_name).fit_function=fit_function;
+            this.FitStruct.(fit_name).fit_tex=fit_tex;
+            this.FitStruct.(fit_name).fit_params=fit_params;
+            this.FitStruct.(fit_name).fit_param_names=fit_param_names;
+            %Generates the anonymous fit function from the above
+            args=['@(x,', strjoin(fit_params,','),')'];
+            anon_fit_fun=str2func(vectorize([args,fit_function]));
+            this.FitStruct.(fit_name).anon_fit_fun=anon_fit_fun;
+        end
+        
+        %Checks if the class is ready to perform a fit
+        function bool=validateData(this)
+            bool=~isempty(this.Data.x) && ~isempty(this.Data.y) && ...
+                length(this.Data.x)==length(this.Data.y) && ...
+                length(this.Data.x)>=this.n_params;
+        end
     end
     
     %% Get and set functions
