f666d41a7f2f5de364d471adcb06fa22a73ca876
Nils Johan Engelsen <nilsjohan.engelsen@gmail.com>
2017-11-28 13:42:46 +0100

    1. Changed MyTrace plot function to take all formatting inputs of the plot and also only conditionally generate labels.

--- @MyTrace/MyTrace.m
+++ @MyTrace/MyTrace.m
@@ -2,10 +2,6 @@ classdef MyTrace < handle & matlab.mixin.Copyable
     properties (Access=public)
         x=[];
         y=[];
-        Color='b';
-        Marker='.';
-        LineStyle='-'
-        MarkerSize=6;
         name_x='x';
         name_y='y';
         unit_x='';
@@ -35,10 +31,6 @@ classdef MyTrace < handle & matlab.mixin.Copyable
             addParameter(p,'filename','placeholder');
             addParameter(p,'x',[]);
             addParameter(p,'y',[]);
-            addParameter(p,'Color','b');
-            addParameter(p,'Marker','none');
-            addParameter(p,'LineStyle','-');
-            addParameter(p,'MarkerSize',6);
             addParameter(p,'unit_x','x');
             addParameter(p,'unit_y','y');
             addParameter(p,'name_x','x');
@@ -193,7 +185,34 @@ classdef MyTrace < handle & matlab.mixin.Copyable
             parse(this.Parser,varargin{:})
             parseInputs(this, false);
         end
-        
+%                 %Set function for Color. Checks if it is a valid color.
+%         function set.Color(this, Color)
+%             assert(iscolor(Color),...
+%                 '%s is not a valid MATLAB default color or RGB triplet',...
+%                 Color);
+%             this.Color=Color;
+%         end
+%         
+%         %Set function for Marker. Checks if it is a valid
+%         %marker style.
+%         function set.Marker(this, Marker)
+%             assert(ismarker(Marker),...
+%                 '%s is not a valid MATLAB MarkerStyle',Marker);
+%             this.Marker=Marker;
+%         end
+%                 %Set function for LineStyle, checks if input is a valid line style.
+%         function set.LineStyle(this, LineStyle)
+%             assert(isline(LineStyle),...
+%                 '%s is not a valid MATLAB LineStyle',LineStyle);
+%             this.LineStyle=LineStyle;
+%         end
+%         
+%         %Set function for MarkerSize, checks if input is a positive number.
+%         function set.MarkerSize(this, MarkerSize)
+%             assert(isnumeric(MarkerSize) && MarkerSize>0,...
+%                 'MarkerSize must be a numeric value greater than zero');
+%             this.MarkerSize=MarkerSize;
+%         end
         %Plots the trace on the given axes, using the class variables to
         %define colors, markers, lines and labels. Takes all optional
         %parameters of the class as inputs.
@@ -206,9 +225,21 @@ classdef MyTrace < handle & matlab.mixin.Copyable
             %Checks that x and y are the same size
             assert(validatePlot(this),...
                 'The length of x and y must be identical to make a plot')
-            %Parses inputs without resetting to defaults
-            parse(this.Parser,varargin{:})
-            parseInputs(this,false);
+            %Parses inputs 
+            p=inputParser();
+            validateColor=@(x) assert(iscolor(x),...
+                'Input must be a valid color. See iscolor function');
+            addParameter(p,'Color','b',validateColor);
+            validateMarker=@(x) assert(ismarker(x),...
+                'Input must be a valid marker. See ismarker function');
+            addParameter(p,'Marker','none',validateMarker);
+            validateLine=@(x) assert(isline(x),...
+                'Input must be a valid linestyle. See isline function');
+            addParameter(p,'LineStyle','-',validateLine);
+            addParameter(p,'MarkerSize',6,...
+                @(x) validateattributes(x,{'numeric'},{'positive'}));
+            addParameter(p,'make_labels',false,@islogical);
+            parse(p,varargin{:});
             
             ind=findLineInd(this, plot_axes);
             if ~isempty(ind) && any(ind)
@@ -219,12 +250,15 @@ classdef MyTrace < handle & matlab.mixin.Copyable
             end
             
             %Sets the correct color and label options
-            set(this.hlines{ind},'Color',this.Color,'LineStyle',...
-                this.LineStyle,'Marker',this.Marker,...
-                'MarkerSize',this.MarkerSize);
-            xlabel(plot_axes,this.label_x,'Interpreter','LaTeX');
-            ylabel(plot_axes,this.label_y,'Interpreter','LaTeX');
-            set(plot_axes,'TickLabelInterpreter','LaTeX');
+            set(this.hlines{ind},'Color',p.Results.Color,'LineStyle',...
+                p.Results.LineStyle,'Marker',p.Results.Marker,...
+                'MarkerSize',p.Results.MarkerSize);
+            
+            if p.Results.make_labels
+                xlabel(plot_axes,this.label_x,'Interpreter','LaTeX');
+                ylabel(plot_axes,this.label_y,'Interpreter','LaTeX');
+                set(plot_axes,'TickLabelInterpreter','LaTeX');
+            end
         end
         
         %If there is a line object from the trace in the figure, this sets
@@ -337,21 +371,7 @@ classdef MyTrace < handle & matlab.mixin.Copyable
     
     %Set and get methods
     methods
-        %Set function for Color. Checks if it is a valid color.
-        function set.Color(this, Color)
-            assert(iscolor(Color),...
-                '%s is not a valid MATLAB default color or RGB triplet',...
-                Color);
-            this.Color=Color;
-        end
-        
-        %Set function for Marker. Checks if it is a valid
-        %marker style.
-        function set.Marker(this, Marker)
-            assert(ismarker(Marker),...
-                '%s is not a valid MATLAB MarkerStyle',Marker);
-            this.Marker=Marker;
-        end
+
         
         %Set function for x, checks if it is a vector of doubles.
         function set.x(this, x)
@@ -365,20 +385,6 @@ classdef MyTrace < handle & matlab.mixin.Copyable
             assert(isnumeric(y),...
                 'Data must be of class double');
             this.y=y(:);
-        end
-        
-        %Set function for LineStyle, checks if input is a valid line style.
-        function set.LineStyle(this, LineStyle)
-            assert(isline(LineStyle),...
-                '%s is not a valid MATLAB LineStyle',LineStyle);
-            this.LineStyle=LineStyle;
-        end
-        
-        %Set function for MarkerSize, checks if input is a positive number.
-        function set.MarkerSize(this, MarkerSize)
-            assert(isnumeric(MarkerSize) && MarkerSize>0,...
-                'MarkerSize must be a numeric value greater than zero');
-            this.MarkerSize=MarkerSize;
         end
         
         %Set function for name, checks if input is a string.
