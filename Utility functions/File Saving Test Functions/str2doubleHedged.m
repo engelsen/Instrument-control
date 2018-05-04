72695891e131eeffc1eea5f3d86a895646273563
Nils Johan Engelsen <nilsjohan.engelsen@gmail.com>
2018-05-04 10:53:21 +0200

    1. Removed deprecated functions

--- Utility functions/File Saving Test Functions/str2doubleHedged.m
+++
@@ -1,9 +0,0 @@
-function [val,str_spec]=str2doubleHedged(str)
-    if ~isnan(str2double(str))
-        val=str2double(str);
-        str_spec='%e';
-    else
-        val=str;
-        str_spec='%s';
-    end
-end
