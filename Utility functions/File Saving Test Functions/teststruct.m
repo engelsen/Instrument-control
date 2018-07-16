2d3d131974bb2baba67281f279edb2778ec10faa
SAFedorov <sergey.fedorov@epfl.ch>
2018-07-16 12:23:52 +0200

    Test functions deleted

--- Utility functions/File Saving Test Functions/teststruct.m
+++
@@ -1,8 +0,0 @@
-clear
-Metadata.Name1.value='x';
-Metadata.Name2.value='y';
-names=fieldnames(Metadata)
-% Metadata=setfield(Metadata,names{:},'str_spec','%s');
-Metadata.(names{1}).str_spec='%s';
-
-Metadata.Name1
