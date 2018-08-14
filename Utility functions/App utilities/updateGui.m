143c217c2179572349a00c160007341dd1e5f125
SAFedorov <sergey.fedorov@epfl.ch>
2018-08-14 11:37:46 +0200

    updateLinkedGuiElements is introduced

--- Utility functions/App utilities/updateGui.m
+++ Utility functions/App utilities/updateGui.m
@@ -1,25 +1,5 @@
-% Set values for all the gui elements listed in app.linked_elem_list
-% according to the properties of an object 
-% Instrument property corresponds to the control element having the same
-% tag as property name, may be a filed of structure or a property of class
-% If specified within the control element OutputProcessingFcn or 
-% InputPrescaler is applied to the property value first
+% This function typically needs to be overloaded in a particular gui
 function updateGui(app)    
-    for i=1:length(app.linked_elem_list)
-        tmpelem = app.linked_elem_list(i);
-        try
-            % update the element value based on app.(tag), 
-            % where tag can contain a reference to sub-objects
-            tmpval = eval(['app.', tmpelem.Tag]);
-            % scale the value if the control element has a prescaler
-            if isprop(tmpelem, 'OutputProcessingFcn')
-                tmpval = tmpelem.OutputProcessingFcn(tmpval);
-            elseif isprop(tmpelem, 'InputPrescaler')
-                tmpval = tmpval*tmpelem.InputPrescaler;
-            end
-            tmpelem.Value = tmpval;
-        catch
-        end
-    end
+    updateLinkedGuiElements(app)
 end
 
---
+++ Utility functions/App utilities/updateLinkedGuiElements.m
@@ -0,0 +1,25 @@
+% Set values for all the gui elements listed in app.linked_elem_list
+% according to the properties of an object 
+% Instrument property corresponds to the control element having the same
+% tag as property name, may be a filed of structure or a property of class
+% If specified within the control element OutputProcessingFcn or 
+% InputPrescaler is applied to the property value first
+function updateLinkedGuiElements(app)    
+    for i=1:length(app.linked_elem_list)
+        tmpelem = app.linked_elem_list(i);
+        try
+            % update the element value based on app.(tag), 
+            % where tag can contain a reference to sub-objects
+            tmpval = eval(['app.', tmpelem.Tag]);
+            % scale the value if the control element has a prescaler
+            if isprop(tmpelem, 'OutputProcessingFcn')
+                tmpval = tmpelem.OutputProcessingFcn(tmpval);
+            elseif isprop(tmpelem, 'InputPrescaler')
+                tmpval = tmpval*tmpelem.InputPrescaler;
+            end
+            tmpelem.Value = tmpval;
+        catch
+        end
+    end
+end
+
