7cd01fe452c8d81c009dcff140037da368375825
SAFedorov <sergey.fedorov@epfl.ch>
2018-09-14 22:41:56 +0200

    printSubnames now properly handles multi-dimensional arrays

--- Utility functions/printSubnames.m
+++ Utility functions/printSubnames.m
@@ -1,31 +1,45 @@
 % Get printed expressions for sub-elements of var as cell array. 
-% Example: if var is a structure array of 2 elements with single fields f1
-% and f2 the function returns 
-% {'(1).f1','(1).f2','(2).f1','(2).f2'}
+% Example: if var is a structure row array of 2 elements with fields
+% f1 and f2 the function returns 
+% {'(1,1).f1','(1,1).f2','(1,2).f1','(1,2).f2'}
 
 function sn = printSubnames(var)
     sn={};
-    if iscell(var)
-        % Expand as cell array
-        arrsz = size(var);
-        ind_str='{';
-        for i=1:length(arrsz)
-            ind_str=[ind_str,'%i,']; %#ok<AGROW>
+    if length(var)>1
+        if iscell(var)
+            % Expand as cell array
+            lbrace='{';
+            rbrace='}';
+        else
+            % Expand as regular array
+            lbrace='(';
+            rbrace=')';
         end
-        ind_str(end)='}';
         
+        % Create structure to address array elements depending on 
+        % the braces type
+        S.type=[lbrace,rbrace];
+        S.subs={};
         
-        for i=1:length(ind_list)
-            ind_str=sprintf(ind_str, ind_list{i});
-            tmp=printSubnames(var{ind_list{i}});
-            tmp=cellfun(@(x)[ind_str, x], tmp, 'UniformOutput', false);
-            sn=[sn; tmp]; %#ok<AGROW>
+        % Create string for prinding formatted indices
+        sz = size(var);
+        ind_fmt=lbrace;
+        for i=1:length(sz)
+            ind_fmt=[ind_fmt,'%i,']; %#ok<AGROW>
         end
-    elseif length(var)>1
-        % Expand as array
-        for i=1:length(var)
-            ind_str=sprintf('(%i)',i);
-            tmp=printSubnames(var(i));
+        ind_fmt(end)=rbrace;
+        
+        % Iterate over array elements
+        ind=cell(1, length(sz));
+        for i=1:numel(var)
+            % Convert 1D to multi-dimensional index
+            [ind{:}]=ind2sub(sz,i);
+            ind_str=sprintf(ind_fmt, ind{:});
+            % Index the variable according to the array type and
+            % print the subnames of its element
+            S.subs={i};
+            tmp=printSubnames(subsref(var,S));
+            % Prepend array indices to the sub-part names
             tmp=cellfun(@(x)[ind_str, x], tmp, 'UniformOutput', false);
             sn=[sn; tmp]; %#ok<AGROW>
         end
@@ -38,6 +52,10 @@ function sn = printSubnames(var)
             tmp=cellfun(@(x)[field_str, x], tmp, 'UniformOutput', false);
             sn=[sn; tmp]; %#ok<AGROW>
         end
+    elseif iscell(var)&&length(var)==1
+        % Expand as single cell
+        tmp=printSubnames(var{1});
+        sn=cellfun(@(x)['{1}', x], tmp, 'UniformOutput', false);
     else
         % Do not expand
         sn={''};
