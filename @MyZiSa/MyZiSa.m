336cf17b285d9e23b972a969be60e6d03c53d176
SAFedorov <sergey.fedorov@epfl.ch>
2019-01-25 17:28:18 +0100

    drafts of MyZiLi (zurich instruments base class) and MyZiSa (scope-based spectrum analyzer) are introduced

---
+++ @MyZiSa/MyZiSa.m
@@ -0,0 +1,20 @@
+% Spectrum analyzer based on Zurich Instruments UHFLI or MFLI
+
+classdef MyZiSa < MyZiLi
+    
+    properties (Access=public)
+        Property1
+    end
+    
+    methods (Access=public)
+        function this = MyZiSa(address)
+            
+        end
+    end
+    
+    %% Set and get methods
+    
+    methods
+    end
+end
+
