20a218059285c7d1178e75ec79fff55ede3815ab
SAFedorov <sergey.fedorov@epfl.ch>
2019-04-05 09:00:21 +0100

    Update MyClassParser. 1) Changed the behavior when a property does not have default - now it is still added to the parser scheme, 2) unmatched_nv property is removed, 3) KeepUnmatched is now 'false' by default, 4) stylistic corrections

--- @MyClassParser/MyClassParser.m
+++ @MyClassParser/MyClassParser.m
@@ -5,16 +5,9 @@
 
 classdef MyClassParser < inputParser
     
-    properties (Dependent=true)
-        unmatched_nv % List of unmatched name-value pairs
-    end
-    
     methods (Access=public)
         function this = MyClassParser(varargin)
             this@inputParser();
-            % KeepUnmatched is true so that the unmatched options could be
-            % passed to another class
-            this.KeepUnmatched=true;
             
             if nargin()==1
                 % If an object is supplied via varargin, add its properties
@@ -26,10 +19,10 @@ classdef MyClassParser < inputParser
         % Add all the properties the class which are not already  present  
         % in the scheme of the parser and which have set access 
         % permitted for MyClassParser 
-        function addClassProperties(this, obj)
-            objMetaclass = metaclass(obj);    
-            for i=1:length(objMetaclass.PropertyList)
-                Tmp = objMetaclass.PropertyList(i);
+        function addClassProperties(this, Obj)
+            ObjMetaclass = metaclass(Obj);    
+            for i=1:length(ObjMetaclass.PropertyList)
+                Tmp = ObjMetaclass.PropertyList(i);
                 
                 % If parameter is already present in the parser scheme,
                 % skip
@@ -46,10 +39,11 @@ classdef MyClassParser < inputParser
                 % Check if the parser has access to the property. This 
                 % can be true in two cases: 1) SetAccess is public 
                 % 2) MyClassParser class was explicitly given access 
-                sa=Tmp.SetAccess;
+                sa = Tmp.SetAccess;
                 if ischar(sa)
-                    has_access=strcmpi(sa,'public');
+                    has_access = strcmpi(sa,'public');
                 elseif iscell(sa)
+                    
                     % Case when SetAcces is specified as cell array of
                     % metaclasses
                     has_access = any(...
@@ -63,30 +57,36 @@ classdef MyClassParser < inputParser
                 if has_access
                     if Tmp.HasDefault
                         def = Tmp.DefaultValue;
+                        
                         % Create validation function based on the class of
                         % default value
-                        val_fcn = @(x)assert(isa(x, class(def)),...
+                        validationFcn = @(x)assert(isa(x, class(def)),...
                             ['The value must be of the class ',class(def),...
                             ' while the present one is of the class ',...
-                            class(x),'.']);
-                        addParameter(this, Tmp.Name, def, val_fcn);
+                            class(x),'.']);                        
+                        opt_vars = {def, validationFcn};
+                    else
+                        def = [];
+                        opt_vars = {def};
                     end
+                    
+                    addParameter(this, Tmp.Name, opt_vars{:});
                 end
             end
         end
-        
-        
+              
         % parse varargin and assign results to class properties 
         % with the same names as parameters 
-        function processInputs(this, obj, varargin)  
+        function processInputs(this, Obj, varargin)  
             parse(this, varargin{:}); 
+            
             % assign results that have associated class properties with
             % public set access
             for i=1:length(this.Parameters)
                 par = this.Parameters{i};
-                if ~ismember(par, this.UsingDefaults) && isprop(obj, par)
+                if ~ismember(par, this.UsingDefaults) && isprop(Obj, par)
                     try
-                        obj.(par) = this.Results.(par);
+                        Obj.(par) = this.Results.(par);
                     catch ME
                         warning(['Value of the input parameter ''' par ...
                             ''' could not be assigned to property. ' ...
@@ -95,15 +95,6 @@ classdef MyClassParser < inputParser
                 end
             end 
         end
-
     end
-    
-  
-    methods 
-        function unmatched_nv=get.unmatched_nv(this)
-            unmatched_nv=struct2namevalue(this.Unmatched);
-        end
-    end
-    
 end
 
