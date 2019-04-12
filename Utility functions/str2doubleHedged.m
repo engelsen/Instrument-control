3482227af8d39913c8381fdbf99615d848c26290
SAFedorov <sergey.fedorov@epfl.ch>
2019-04-12 15:19:04 +0200

    Debugging of readTrace of MyTekScope

--- Utility functions/str2doubleHedged.m
+++ Utility functions/str2doubleHedged.m
@@ -1,10 +1,11 @@
-function [val,str_spec]=str2doubleHedged(str)
-    conv_str=str2double(str);
+function [val, str_spec] = str2doubleHedged(str)
+
+    conv_str = str2double(str);
     if ~isnan(conv_str)
-        val=conv_str;
-        str_spec='%e';
+        val = conv_str;
+        str_spec = '%e';
     else
-        val=str;
-        str_spec='%s';
+        val = str;
+        str_spec = '%s';
     end
 end
