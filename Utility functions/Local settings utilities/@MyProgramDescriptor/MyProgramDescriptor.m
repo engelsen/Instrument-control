985525dafcd1f30c217551fac4b1fd06afad4eb4
SAFedorov <sergey.fedorov@epfl.ch>
2019-04-22 12:12:05 +0200

    Introduced MyProgramDescriptor class

---
+++ Utility functions/Local settings utilities/@MyProgramDescriptor/MyProgramDescriptor.m
@@ -0,0 +1,33 @@
+% Descriptor for local instrument-control programs
+
+classdef MyProgramDescriptor    
+    properties (Access = public)
+        name        = ''     % Identifier that is a MATLAB variable name
+        title       = ''     % Title displayed in menus
+        info        = ''     % Description
+        type        = ''     % runfile/instrument/logger
+        enabled     = true
+        data_source = true   % If program produces traces
+        run_expr    = ''     % Expression to run the program
+        run_bg_expr = ''     % Expression to run the program without GUI
+    end
+    
+     methods (Access = public)
+        function this = MyProgramDescriptor(varargin)
+            P = MyClassParser(this);
+            processInputs(P, this, varargin{:});
+        end
+     end
+     
+     methods 
+         function this = set.name(this, val)
+             assert(isvarname(val), '''name'' must be valid variable name')
+             this.name = val;
+             
+             if isempty(this.title) %#ok<*MCSUP>
+                 this.title = val; 
+             end
+         end
+     end
+end
+
