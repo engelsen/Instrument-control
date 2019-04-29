3c75ad55592a2ca3fd84a732aa12d0bc366b5761
SAFedorov <sergey.fedorov@epfl.ch>
2019-04-29 16:07:18 +0200

    Update MyColdEdgeCryo.m

--- @MyColdEdgeCryo/MyColdEdgeCryo.m
+++ @MyColdEdgeCryo/MyColdEdgeCryo.m
@@ -2,7 +2,7 @@
 % The manifold is managed by an Arduino board that communicates with 
 % computer via serial protocol. 
 
-classdef MyCeCryo < MyScpiInstrument
+classdef MyColdEdgeCryo < MyScpiInstrument & MyCommCont
     
     properties (GetAccess = public, SetAccess = protected, ...
             SetObservable = true)
@@ -13,7 +13,7 @@ classdef MyCeCryo < MyScpiInstrument
         
         % Time for the recirculator to pump down helium from the capillary 
         % before closing it off
-        tr = 600
+        tr = 900
     end
     
     properties (Access = protected)
@@ -21,15 +21,17 @@ classdef MyCeCryo < MyScpiInstrument
     end
     
     methods (Access = public)
-        function this = MyCeCryo(interface, address, varargin)
-            this@MyScpiInstrument(interface, address, varargin{:});
+        function this = MyColdEdgeCryo(varargin)
+            this@MyCommCont(varargin{:});
             
             this.Timer = timer();
             
             % Buffer size of 64 kB should be way an overkill. The labview
             % program provided by ColdEdge use 256 Bytes.
-            this.Device.InputBufferSize=2^16;
-            this.Device.OutputBufferSize=2^16;
+            this.Device.InputBufferSize = 2^16;
+            this.Device.OutputBufferSize = 2^16;
+            
+            createCommandList(this);
         end
         
         function delete(this)
@@ -51,45 +53,60 @@ classdef MyCeCryo < MyScpiInstrument
             stop(this.Timer);
             this.operation_in_progress = false;
             
-            writePropertyHedged(this, ...
-                'valve1',       false, ...
-                'valve2',       false, ...
-                'valve3',       false, ...
-                'valve4',       false, ...
-                'valve5',       false, ...
-                'valve7',       false, ...
-                'recirc',       false, ...
-                'cryocooler',   false);
+            this.auto_sync = false;
+            
+            this.valve1 = false;
+            this.valve2 = false;
+            this.valve3 = false;
+            this.valve4 = false;
+            this.valve5 = false;
+            this.valve7 = false;
+            this.recirc = false;
+            this.cryocooler = false;
+            
+            % Sync once
+            sync(this);
+            
+            % Return to the hedged mode
+            this.auto_sync = true;
         end
         
         function startCooldown(this)
             assert(~this.operation_in_progress, ['Cannot initiate' ...
                 ' cooldown stop. Another operation is in progress.'])
             
-            writePropertyHedged(this, ...
-                'valve2',       false, ...
-                'valve3',       false, ...
-                'valve5',       false, ...
-                'valve7',       false);
+            this.auto_sync = false;
             
-            writePropertyHedged(this, ...
-                'valve1',       true, ...
-                'valve4',       true, ...
-                'recirc',       true, ...
-                'cryocooler',   true);
+            this.valve2 = false;
+            this.valve3 = false;
+            this.valve5 = false;
+            this.valve7 = false;
+            
+            % Open the recirculator path, starting from the return
+            this.valve4 = true;
+            this.valve1 = true;
+            
+            % Start the compressors
+            this.recirc = true;
+            this.cryocooler = true;
+            
+            % Sync once
+            sync(this);
+            
+            % Return to the hedged mode
+            this.auto_sync = true;
         end
         
         function stopCooldown(this)
             function switchRecirculatorOff(~, ~)
-                writePropertyHedged(this, ...
-                    'valve1',       false, ...
-                    'valve2',       false, ...
-                    'valve3',       false, ...
-                    'valve4',       false);
+                
+                % Close the recirculator path, starting from the supply
+                this.valve1 = false;
+                this.valve4 = false;
                 
                 % Switch off the recirculator after all the valves are
                 % closed
-                writePropertyHedged(this, 'recirc', false);
+                this.recirc = false;
                 
                 this.operation_in_progress = false;
             end
@@ -98,13 +115,9 @@ classdef MyCeCryo < MyScpiInstrument
                 ' cooldown stop. Another operation is in progress.'])
             
             % Switch off the cryocooler, close the recirculator supply 
-            % valve (1) and at the same time open the valves bridging the 
-            % recirculator return and supply (2 and 3).
-            writePropertyHedged(this, ...
-                'valve1',       false, ...
-                'valve2',       true, ...
-                'valve3',       true, ...
-                'cryocooler',   false);
+            % valve (1).
+            this.valve1 = false;
+            this.cryocooler = false;
             
             % Wait for the helium to be pumped out of the capillary by the
             % recirculator and then switch the recirculator off
@@ -121,42 +134,35 @@ classdef MyCeCryo < MyScpiInstrument
         function createCommandList(this)
             
             % Valve states
-            for i=1:7
+            for i = 1:7
                 if i == 6
-                    continue % No valve 6
+                    continue % There is no valve 6
                 end
                 
                 tag = sprintf('valve%i',i);
                 cmd = sprintf(':VALVE%i',i);
-                addCommand(this, tag, cmd,...
-                    'default',  false, ...
-                    'fmt_spec', '%b', ...
+                
+                addCommand(this, tag, cmd, ...
+                    'format',   '%b', ...
                     'info',     'Valve open(true)/clsed(false)');
             end
             
-            addCommand(this, 'recirc', ':REC',...
-                'default',  false, ...
-                'fmt_spec', '%b', ...
+            addCommand(this, 'recirc', ':REC', ...
+                'format',   '%b', ...
                 'info',     'Recirculator on/off');
             
-            addCommand(this, 'cryocooler', ':COOL',...
-                'default',  false, ...
-                'fmt_spec', '%b', ...
+            addCommand(this, 'cryocooler', ':COOL', ...
+                'format',   '%b', ...
                 'info',     'Cryocooler on/off');
             
-            addCommand(this, 'press', ':PRES',...
-                'default',  false, ...
-                'fmt_spec', '%e', ...
+            addCommand(this, 'press', ':PRES', ...
+                'format',   '%e', ...
                 'access',   'r', ...
                 'info',     'Supply pressure (PSI)');
         end
     end
     
-    methods
-        function set.operation_in_progress(this, ~)
-            notify(this, 'PropertyRead');
-        end
-        
+    methods        
         function val = get.operation_in_progress(this)
             try
                 val = strcmpi(this.Timer.Running, 'on');
