ddfc1e3685f5b9ae03e1fb9599f220ae55499925
SAFedorov <sergey.fedorov@epfl.ch>
2019-09-24 18:17:21 +0200

    Moved remaining initial parameter estimation functions within the fit classes and deleted the standalone versions

--- Fit classes/@MyGorodetsky2000Fit/MyGorodetsky2000Fit.m
+++ Fit classes/@MyGorodetsky2000Fit/MyGorodetsky2000Fit.m
@@ -18,9 +18,36 @@ classdef MyGorodetsky2000Fit < MyFit
     
     methods (Access=protected)
         %Calculates the initial parameters using an external function.
-        function [init_params,lim_lower,lim_upper]=calcInitParams(this)
-            [init_params,lim_lower,lim_upper]=...
-                initParamGorodetsky2000(this.Data.x,this.Data.y);
+        function [p_in,lim_lower,lim_upper]=calcInitParams(this)
+            x = this.Data.x;
+            y = this.Data.y;
+            
+            %             { 'a','b','c', 'gamma','k0', 'kex'},...
+            lim_upper=[Inf,Inf,Inf,Inf,Inf,Inf];
+            lim_lower=[-Inf,-Inf,-Inf,0,0,0];
+
+
+            %Finds peaks on the negative signal (max 2 peaks)
+            [~,locs,widths,~]=findpeaks(-y,x,...
+                'MinPeakDistance',0.001*range(x),'SortStr','descend','NPeaks',2);
+
+
+            p_in(1)=max(y);
+
+            %position
+            p_in(2)=mean(locs);
+
+            p_in(3)=(y(end)-y(1))/(x(end)-x(1));
+
+            if length(locs)==2
+                p_in(4)=abs(diff(locs))/2;
+            else
+                p_in(4)=0;
+            end
+
+            p_in(5)=mean(widths)/2;
+            %Assume critical coupling
+            p_in(6)=p_in(4);
         end
         
         function genSliderVecs(this)
